a598fdf5d4c41e8e066149f4267ba34cb8b5f5ac
Lisa Milena Fernández Ayala <mile_etp@hotmail.com>
2021-11-04 21:26:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#### run project
+compile:
+```sh
+$ npm install
+```
+build:
+```sh
+$ npm run build
+```
+run:
+```sh
+$ npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+            "style-loader", 
+            "css-loader", 
+            "sass-loader"
+        ]
+    }]
+}
+};
